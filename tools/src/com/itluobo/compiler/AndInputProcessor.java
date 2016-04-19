0a17832bf6dd98579fdd42529f597d356bf3911d
Zhu Weijie <kenvi.zhu@dianping.com>
2016-04-19 18:21:37 +0800

    add frame

---
+++ tools/src/com/itluobo/compiler/AndInputProcessor.java
@@ -0,0 +1,12 @@
+package com.itluobo.compiler;
+
+/**
+ * Created by kenvi on 16/4/19.
+ */
+public class AndInputProcessor implements InputProcessor{
+
+    @Override
+    public State process(String input) {
+        return null;
+    }
+}
